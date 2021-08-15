3926cce523c3f53914170d5ffc3e99ec7823204c
Jacob Baird <jhackgpu@gmail.com>
2021-08-15 19:41:45 -0300

    Generate images for comments

--- bin/get_post.py
+++ bin/get_post.py
@@ -1,8 +1,16 @@
 import requests
 import json
 import sys
+from PIL import Image
+from PIL import ImageFont
+from PIL import ImageDraw
 
 URL = sys.argv[1]
+
+IMAGE_WIDTH=900
+IMAGE_HEIGHT=900
+FONT_SIZE=18
+COMMENT_FONT_SIZE=FONT_SIZE * 2
 
 # get cookie for request
 a_session = requests.Session()
@@ -39,7 +47,6 @@ for comment_key in models_json.keys():
         comment_dict = {}
         comment_dict['id'] = comment_json['id']
         comment_dict['author'] = comment_json['author']
-        comment_dict['parentId'] = comment_json['parentId']
         comment_dict['score'] = comment_json['score']
         media_json = comment_json['media']
         rtc_json = media_json['richtextContent']
@@ -48,10 +55,32 @@ for comment_key in models_json.keys():
         c_json = c_arr['c']
         e_arr = c_json[0]
         comment_dict['text'] = e_arr['t']
-        comments_arr.append(comment_dict)
+        if comment_json['parentId'] == None:
+            comments_arr.append(comment_dict)
     except:
         error_count = error_count + 1
 
-# output comment array
+font = ImageFont.truetype("res/Consolas.ttf", FONT_SIZE)
+comment_font = ImageFont.truetype("res/Consolas.ttf", COMMENT_FONT_SIZE)
 
-print(comments_arr)
+for comment in comments_arr:
+    img = Image.new(mode="RGB", size = (IMAGE_WIDTH, IMAGE_HEIGHT), color = (8, 8, 8))
+    draw = ImageDraw.Draw(img)
+    font = ImageFont.truetype("res/Consolas.ttf", FONT_SIZE)
+    draw.text((10, 10), "u/" + comment['author'] + "@effortless.reddit.curator:~/r/ask_reddit$", (20, 200, 20), font=font)
+    draw.text((10, IMAGE_HEIGHT - 30), "\"up votes\": " + str(comment['score']), (20, 200, 20), font=font)
+    tokens = comment['text'].split(' ')
+    x = 10
+    y = 40 + COMMENT_FONT_SIZE
+    for token in tokens:
+        if x > IMAGE_WIDTH * 0.6:
+            x = 10
+            y = y + COMMENT_FONT_SIZE
+        if x == 10:
+            draw.text((x, y), ">", (170, 170, 170), font=comment_font)
+            x = x + COMMENT_FONT_SIZE * 0.55 * 2
+        draw.text((x, y), token, (170, 170, 170), font=comment_font)
+        x = x + (len(token) + 1) * COMMENT_FONT_SIZE * 0.55
+            
+    file_name = "data/img_" + comment['id'] + ".png"
+    img.save(file_name)
--- curate.sh
+++ curate.sh
@@ -2,4 +2,7 @@ echo "URL of Reddit post: $1"
 
 echo "Scraping Reddit page..."
 
+mkdir data
 python3 bin/get_post.py $1 > data/post.json
+
+echo "Constructing comment tree..."
