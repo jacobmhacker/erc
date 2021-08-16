f0a03d445fe0ffe9bd4cc32ece5e947ed0ff6677
Jacob Baird <jhackgpu@gmail.com>
2021-08-15 22:14:05 -0300

    Generate videos

--- bin/get_post.py
+++ bin/get_post.py
@@ -1,9 +1,11 @@
 import requests
 import json
 import sys
+import os
 from PIL import Image
 from PIL import ImageFont
 from PIL import ImageDraw
+from gtts import gTTS
 
 URL = sys.argv[1]
 
@@ -13,16 +15,22 @@ FONT_SIZE=18
 COMMENT_FONT_SIZE=FONT_SIZE * 2
 
 # get cookie for request
+print("Getting cookie from Reddit")
+
 a_session = requests.Session()
 a_session.get("https://www.reddit.com/")
 session_cookies = a_session.cookies
 cookies_dictionary = session_cookies.get_dict()
 
 # submit request for reddit post
+print("Requesting Reddit post")
+
 r = requests.get(url = URL, cookies=cookies_dictionary)
 r_body = r.text
 
 # extract comments from reddit post
+print("Extracting comments")
+
 comments_start = r_body.find("<script id=\"data\">")
 comments_end = r_body.find("</script>", comments_start)
 comments_body = r_body[comments_start:comments_end]
@@ -37,6 +45,7 @@ comments_json = features_json['comments']
 models_json = comments_json['models']
 
 # convert comments to json array
+print("Parsing comments")
 
 comments_arr = []
 error_count = 0
@@ -64,6 +73,8 @@ font = ImageFont.truetype("res/Consolas.ttf", FONT_SIZE)
 comment_font = ImageFont.truetype("res/Consolas.ttf", COMMENT_FONT_SIZE)
 
 for comment in comments_arr:
+    print("Rendering image for comment " + comment['id'])
+
     img = Image.new(mode="RGB", size = (IMAGE_WIDTH, IMAGE_HEIGHT), color = (8, 8, 8))
     draw = ImageDraw.Draw(img)
     font = ImageFont.truetype("res/Consolas.ttf", FONT_SIZE)
@@ -84,3 +95,26 @@ for comment in comments_arr:
             
     file_name = "data/img_" + comment['id'] + ".png"
     img.save(file_name)
+
+    print("Dubbing audio for comment " + comment['id'])
+
+    audio_name = "data/audio_" + comment['id'] + ".mp3"
+    comment_audio = gTTS(text=comment['text'], lang='en', tld='ca', slow=False)
+    comment_audio.save(audio_name)
+
+    print("Generating video for comment " + comment['id'])
+
+    video_name = "data/video_" + comment['id'] + ".mp4"
+
+    os.system('ffmpeg -loop 1 -i ' + file_name + ' -i ' + audio_name + ' -c:v libx264 -tune stillimage -c:a aac -b:a 192k -pix_fmt yuv420p -shortest ' + video_name + ' > /dev/null 2>&1')
+    os.system('echo file ' + video_name[5:] + ' >> data/vid_list.txt')
+
+    print("Clearing image and audio files for comment " + comment['id'])
+    os.system('rm ' + file_name + ' ' + audio_name)
+
+print("Concatenating videos")
+os.system('ffmpeg -f concat -i data/vid_list.txt -c copy final_video.mp4 > /dev/null 2>&1')
+
+print("Removing intermediate videos")
+os.system('rm -f data/vid*')
+os.system('rm -r data')
--- curate.sh
+++
@@ -1,8 +0,0 @@
-echo "URL of Reddit post: $1"
-
-echo "Scraping Reddit page..."
-
-mkdir data
-python3 bin/get_post.py $1 > data/post.json
-
-echo "Constructing comment tree..."
